4adcb11df3dcda60c1bbff1bab99b5ea26f92fe6
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-19 11:58:00 +0200

    Create a Custom "Access Denied" Page - Configuration

--- src/main/java/com/tilmeez/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/tilmeez/springsecurity/demo/config/DemoSecurityConfig.java
@@ -58,6 +58,9 @@ public class DemoSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
                 .logout(configure ->
                         configure
                                 .permitAll())
+                .exceptionHandling(configurer ->
+                        configurer
+                                .accessDeniedPage("/access-denied"))
                 .build();
     }
 
